05fc315139e653a32b5e64d47d846715d01006c5
dezash123 <desmondmehta@gmail.com>
2025-08-03 15:24:25 -0400

    trying to get display working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,37 +6,25 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use bt_hci::cmd::info;
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::systimer::SystemTimer;
-use esp_hal::timer::timg::TimerGroup;
-use esp_wifi::ble::controller::BleConnector;
-use bt_hci::controller::ExternalController;
-use co5300::co5300::Co5300;
-use esp_hal::spi::master::{Config, Spi};
-use esp_hal::spi::Mode;
+use esp_hal::spi::master::{Address, Command, Config, Spi};
+use esp_hal::spi::DataMode;
 use esp_hal::time::Rate;
 use esp_hal::gpio::Flex;
 
 use defmt_rtt as _;
 use defmt::info;
 
+use embassy_time::{Timer};
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-
 use panic_rtt_target as _;
-use trouble_host::peripheral;
 
 extern crate alloc;
 
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
-
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    // generator version: 0.4.0
-
     rtt_target::rtt_init_defmt!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -51,48 +39,139 @@ async fn main(spawner: Spawner) {
 
     info!("Embassy initialized!");
 
-    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
-    let timer1 = TimerGroup::new(peripherals.TIMG0);
-    let wifi_init = esp_wifi::init(timer1.timer0, rng, peripherals.RADIO_CLK)
-        .expect("Failed to initialize WIFI/BLE controller");
-    let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
-        .expect("Failed to initialize WIFI controller");
-    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
-    let transport = BleConnector::new(&wifi_init, peripherals.BT);
-    let _ble_controller = ExternalController::<_, 20>::new(transport);
-
     // TODO: Spawn some tasks
     let _ = spawner;
+    
+    info!("Hello world!");
 
     let mut spi = Spi::new(
-    peripherals.SPI2,
-    Config::default()
-        .with_frequency(Rate::from_khz(100))
-        .with_mode(Mode::_0),
+        peripherals.SPI2,
+        Config::default()
+        .with_frequency(Rate::from_mhz(10))
     ).unwrap()
     .with_sck(peripherals.GPIO12)
     .with_sio0(peripherals.GPIO11)
-    .with_sio1(peripherals.GPIO13)
-    .with_sio2(peripherals.GPIO14)
-    .with_sio3(peripherals.GPIO9)
-    .with_cs(peripherals.GPIO10)
-    .into_async();
+    // .with_sio1(peripherals.GPIO13)
+    // .with_sio2(peripherals.GPIO13)
+    // .with_sio3(peripherals.GPIO13)
+    .with_cs(peripherals.GPIO10);
+    //.into_async();
 
-    let rst = Flex::new(peripherals.GPIO2);
-    let te = Flex::new(peripherals.GPIO1);
+    let mut rst = Flex::new(peripherals.GPIO1);
+    info!("resetting");
+    rst.set_low();
+    Timer::after_millis(500).await;
+    rst.set_high();
+    Timer::after_millis(500).await;
 
-    let mut co5300 = Co5300::new(spi, te, rst).await.unwrap();
-    info!("Hello world!");
+    info!("resetting spi mode");
+    // reset to sspi mode
+    spi.half_duplex_write(
+        DataMode::Single,
+        Command::_8Bit(0xFF, DataMode::Single),
+        Address::None,
+        0,
+        &[],
+    ).unwrap();
+    Timer::after_millis(500).await;
+    
+    info!("reading");
+    let mut buf = [0u8; 3];
+    spi_read(&mut spi, 0x04, &mut buf).unwrap();
+    info!("id: {:?}", buf);
+    Timer::after_secs(1).await;
+
+    spi_write(&mut spi, 0xFE, &[0x00]).unwrap();
+    spi_write(&mut spi, 0xC4, &[0x80]).unwrap();
+    spi_write(&mut spi, 0x3A, &[0x77]).unwrap();
+    // spi_write(&mut spi, 0x35, &[0x00]).unwrap();
+    spi_write(&mut spi, 0x34, &[0x00]).unwrap(); // TEOFF
+    spi_write(&mut spi, 0x53, &[0x20]).unwrap();
+    spi_write(&mut spi, 0x51, &[0xFF]).unwrap();
+    spi_write(&mut spi, 0x63, &[0xFF]).unwrap();
+    spi_write(&mut spi, 0x2A, &[0x00, 240, 0, 255]).unwrap();
+    spi_write(&mut spi, 0x2B, &[0x00, 240, 0, 255]).unwrap();
+    spi_write(&mut spi, 0x11, &[]).unwrap();
+    Timer::after_millis(500).await;
+    spi_write(&mut spi, 0x29, &[]).unwrap();
+    info!("init done");
+    Timer::after_secs(1).await;
+
+    let mut buf = [0u8; 1];
+    spi_read(&mut spi, 0x0A, &mut buf).unwrap();
+    info!("disply power mode: {:08b}", buf[0]);
+
+    let mut buf = [0u8; 1];
+    spi_read(&mut spi, 0x54, &mut buf).unwrap();
+    info!("brightness mode: {:08b}", buf[0]);
+
+    let mut buf = [0u8; 1];
+    spi_read(&mut spi, 0x0E, &mut buf).unwrap();
+    info!("display signal mode: {:08b}", buf[0]);
     loop {
-        Timer::after(Duration::from_secs(1)).await;
-        info!("turning on");
-        co5300.all_pixels_on().await.unwrap();
-        info!("turned on");
-        Timer::after(Duration::from_secs(1)).await;
-        info!("turning off");
-        co5300.all_pixels_off().await.unwrap();
-        info!("turned off");
+        info!("on");
+        spi_write(&mut spi, 0x23, &[]).unwrap();
+        Timer::after_secs(2).await;
+        info!("off");
+        spi_write(&mut spi, 0x22, &[]).unwrap();
+        Timer::after_secs(2).await;
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.1/examples/src/bin
 }
+
+fn spi_read<Dm: esp_hal::DriverMode>(spi: &mut Spi<Dm>, addr: u8, buf: &mut [u8]) -> Result<(), esp_hal::spi::Error> {
+    info!("spi read: {}", addr);
+    spi.half_duplex_read(
+        DataMode::Single,
+        Command::_8Bit(0x03, DataMode::Single),
+        Address::_24Bit(u32::from_be_bytes([0x00, 0x00, addr, 0x00]), DataMode::Single),
+        0,
+        buf,
+    )
+}
+
+fn spi_write<Dm: esp_hal::DriverMode>(spi: &mut Spi<Dm>, addr: u8, data: &[u8]) -> Result<(), esp_hal::spi::Error> {
+    info!("spi write: {}: {:?}", addr, data);
+    spi.half_duplex_write(
+        DataMode::Single,
+        Command::_8Bit(0x02, DataMode::Single),
+        Address::_24Bit(u32::from_be_bytes([0x00, 0x00, addr, 0x00]), DataMode::Single),
+        0,
+        data,
+    )
+}
+
+
+// fn to_point(data: [u8; 3]) -> (u16, u16) {
+//     let x = u16::from_be_bytes([(data[0] >> 6) & 1, data[1]]);
+//     let y = u16::from_be_bytes([(data[0] >> 7) & 1, data[2]]);
+//     (x, y)
+// }
+// 
+// async fn touch_stuff(peripherals: Peripherals) {
+//     let mut i2c = I2c::new(
+//         peripherals.I2C0,
+//         I2cConfig::default()
+//             .with_frequency(Rate::from_khz(200))
+//     ).unwrap()
+//     .with_sda(peripherals.GPIO6)
+//     .with_scl(peripherals.GPIO5)
+//     .into_async();
+// 
+//     let mut int = Flex::new(peripherals.GPIO7);
+//     int.apply_input_config(&InputConfig::default().with_pull(Pull::Up));
+// 
+//     loop {
+//         int.wait_for_low().await;
+//         info!("falling edge");
+//         let mut buf = [0u8; 3];
+//         match i2c.read(0x2E, &mut buf) {
+//             Ok(_) => {
+//                 info!("point: {:?}", to_point(buf));
+//             },
+//             Err(e) => {
+//                 info!("error: {:?}", e);
+//             }
+//         }
+//         int.wait_for_high().await;
+//     }
+// }
